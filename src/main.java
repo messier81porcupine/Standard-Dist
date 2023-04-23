d6b70071a6038771877d8bf4a8fcf2399b3403d2
Elliot HH <eharhar06@proton.me>
2023-04-23 15:20:31 -0700

    Added comments
    Both main.java and Sim.java are not explained in the comments
    Removed old commented out lines

--- src/main.java
+++ src/main.java
@@ -5,13 +5,16 @@ public class main {
     throws IOException
     {
         Sim sim = new Sim();
+        //for reading lines
         BufferedReader bfn = new BufferedReader(
             new InputStreamReader(System.in));
         
+        //Get num of balls from user (also decides how many bins there will be, bounces * 2 = num of bins)
         System.out.println("Enter the Number of Balls to Run: ");
         String numBalls = bfn.readLine();
         int totalIterations = Integer.parseInt(numBalls);
 
+        //Get num of rows for the balls to bounce on from user
         System.out.println("Enter the Number of Bounces per Ball: ");
         String numBounces = bfn.readLine();
         int totalBounces = Integer.parseInt(numBounces);

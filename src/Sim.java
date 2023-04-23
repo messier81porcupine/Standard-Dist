df05cd5fec48cb038995516e1fcacbff5de7944b
Elliot HH <eharhar06@proton.me>
2023-04-23 14:15:07 -0700

    initial work
    makes sense and seems to work. only tested with small nums

---
+++ src/Sim.java
@@ -0,0 +1,50 @@
+import java.lang.reflect.Array;
+import java.util.Random;
+import java.util.HashMap;
+
+public class Sim {
+    public HashMap runBall(int totalIterations, int totalBounces){ // totalIterations = num of balls to run // bounces should always be odd
+        HashMap<Integer, Integer> bins = new HashMap<Integer, Integer>();
+
+        for (int i = totalBounces * -1; i <= totalBounces; i ++){
+            System.out.println(i);
+            if (i % 2 != 0){
+                bins.put(i,0);
+            }
+            
+            System.out.println(bins);
+        }    
+        
+        //Random rand = new Random();
+        for (int iteration = 0; iteration < totalIterations; iteration ++){ // for each ball
+            int dir = 0;
+            int pos = 0;
+            System.out.println("\nBall #" + iteration);
+            
+
+            for (int bounce = 0; bounce < totalBounces; bounce ++){ // for each bounce
+                //decide which way ball goes
+                dir = 0;
+                double num = Math.random();
+            
+                if (num <= 0.5){
+                    dir = -1;
+                }
+                else if (num >= 0.5) { 
+                    dir = 1;
+                }
+                System.out.println("Bounce #" + bounce);
+                System.out.println(pos + " " + dir);
+                pos += dir;
+                
+                System.out.println("Pos " + pos);
+            }
+            
+            
+            bins.put(pos, bins.get(pos) + 1); //place ball into its bin (decided by pos) via incrementing that bin
+
+
+        }
+    return bins;
+    }   
+}
---
+++ src/main.java
@@ -0,0 +1,9 @@
+
+public class main {
+    public static void main(String[] args) {
+        Sim sim = new Sim();
+        System.out.println(sim.runBall(10, 5));
+        //sim.runBall(10, 5);
+    }
+    
+}
